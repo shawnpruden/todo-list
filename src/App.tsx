361348b145eebb80128ff3617836c347534bd39d
shawnpruden <echt.shawn@gmail.com>
2024-11-28 16:32:57 +0900

    fixed route issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';
 import Auth from './pages/Auth';
 import { ReactNode } from 'react';
-import { AuthProvider } from './hooks/useAuth';
+import useAuth, { AuthProvider } from './hooks/useAuth';
 import Dashboard from './pages/Dashboard';
 
 function App(): ReactNode {
+  const PrivateRoute = ({ children }: { children: ReactNode }) => {
+    const { user } = useAuth();
+    return user ? children : <Navigate to="/login" replace />;
+  };
+
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
 
         <Route path="/login" element={<Auth />} />
         <Route path="/sign-up" element={<Auth />} />
